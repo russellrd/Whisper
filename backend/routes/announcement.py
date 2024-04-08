d1d2ca3a182bfebb1631d1d49d3f0e9f1be75545
Mehdi Syed <m3hd1.syed@gmail.com>
2024-04-07 21:55:27 -0400

    Backend route for create channel page, testing req.

--- backend/routes/announcement.py
+++ backend/routes/announcement.py
@@ -1,16 +1,22 @@
 from flask import Blueprint, request
 from database.database_handler import database_command 
+import secrets
 
 announcement_channel_bp = Blueprint("announcementChannel", __name__, url_prefix="/announcementChannel")
 
 @announcement_channel_bp.route("/getAll", methods=["GET"])
 def get_all_announcements():
-    return database_command("SELECT * FROM ANNOUNCEMENT_CHANNELS;")
+    return database_command(f"SELECT * FROM ANNOUNCEMENT_CHANNELS;")
 
 def get_user_annoucements():
     user_id = request.args.get("id")
-    return database_command("""SELECT Ac.id, Ac.title, Ac.department 
+    return database_command(f"""SELECT Ac.id, Ac.title, Ac.department 
                             FROM ANNOUNCMENTS_SUBS As, ANNOUNCEMENT_CHANNELS Ac,
                             WHERE {user_id} = As.userID
                                 AND As.annoucementID = Ac.id""")
-    
+
+@announcement_channel_bp.route("/createAnnouncementPage", methods=["POST"])
+def create_announcement_channel():
+    id = secrets.token_bytes(16).hex()
+    title = request.args("title")
+    return database_command(f"INSERT INTO ANNOUNCEMENT_CHANNELS (id, title, department) VALUES ('{id}', '{1}', '{NULL}')")
